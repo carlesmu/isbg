39a649498d7e81c8e2536a938110ab0435aad4a1
Carles Muñoz Gorriz <carlesmu@internautas.org>
2018-01-01 20:57:32 +0100

    Added more tests.

--- tests/test_sa_unwrap.py
+++ tests/test_sa_unwrap.py
@@ -23,31 +23,97 @@
 
 import os
 import sys
+import email.message
 
 # We add the upper dir to the path
 sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))
+from isbg import sa_unwrap  # noqa: E402
+
+try:
+    PARSE_FILE = email.message_from_binary_file  # Python3
+    MESSAGE = email.message_from_bytes           # Python3
+except AttributeError:
+    PARSE_FILE = email.message_from_file         # Python2
+    MESSAGE = email.message_from_string          # Python2+3
 
 
-def test_unwrap_spam_from_spamassassin():
+def test_sa_unwrap_from_email():
+    """Test function sa_unwrap_from_email."""
+    """
+    Test parsing a file with a message no multipart. It should return none.
+    """
+    f = open('examples/spam.eml', 'rb')
+    ret = sa_unwrap.sa_unwrap_from_email(PARSE_FILE(f))
+    f.close()
+    assert ret is None, "%r is not a None." % ret
+
+    """
+    Test creating a multipart missage with 1 message from a file.
+    It should return 1 Message.
+    """
+    f = open('examples/spam.from.spamassassin.eml', 'rb')
+    ftext = f.read()
+    ret = sa_unwrap.sa_unwrap_from_email(MESSAGE(ftext))
+    f.close()
+    num_mails = 0
+    for mail in ret:
+        assert isinstance(mail, email.message.Message), \
+            "%r is not a email.message.Message[]." % ret
+        num_mails = num_mails + 1
+    assert num_mails == 1, "%d mails found" % num_mails
+
+    if sys.version_info[0] > 3:  # Only python 3
+        """
+        Test a very simple message created from his bytes.
+        It should return None.
+        """
+        # pylint: disable=no-member
+        ret = sa_unwrap.sa_unwrap_from_email(email.message_from_bytes(b'0000'))
+        assert ret is None, "%r is not a None." % ret
+
+    """
+    Test a very simple message created from his strings.
+    It should return None.
+    """
+    ret = sa_unwrap.sa_unwrap_from_email(email.message_from_string('None'))
+    assert ret is None, "%r is not a None." % ret
+
+
+def test_unwrap():
+    """Test funcition unwrap."""
     """Test unwrap of examples/spam.from.spamassassin.eml --> 1 message."""
-    from isbg import sa_unwrap
-    import email.message
     f = open('examples/spam.from.spamassassin.eml', 'rb')
     mails = sa_unwrap.unwrap(f)
     f.close()
     num_mails = 0
     for mail in mails:
         assert isinstance(mail, email.message.Message), \
-            "%r is not a email.message.Message." % mails
+            "%r is not a email.message.Message[]." % mails
         num_mails = num_mails + 1
     assert num_mails == 1, "%d mails found" % num_mails
 
-
-def test_unwrap_spam():
     """Test unwrap of examples/spam.eml --> None."""
-    from isbg import sa_unwrap
     f = open('examples/spam.eml', 'rb')
     mails = sa_unwrap.unwrap(f)
     f.close()
     assert mails is None
+
+
+def test_run_1(capsys):
+    """Test no multipart spam mail."""
+    f = open('examples/spam.eml').read()
+    sys.stdin = f
+    sa_unwrap.run()
+    out, err = capsys.readouterr()
+    sa_unwrap.run()
+    assert out == "No spam into the mail detected.\n"
+
+
+def test_run_2(capsys):
+    """Test multipart spam mail. Returns the multipart message."""
+    f = open('examples/spam.from.spamassassin.eml').read()
+    sys.stdin = f
+    sa_unwrap.run()
+    out, err = capsys.readouterr()
+    assert out.startswith("Return-Path: ")
