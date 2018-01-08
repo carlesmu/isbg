f8dc47a98826f33a7181de0f4df84177d9ce9f98
Carles Muñoz Gorriz <carlesmu@internautas.org>
2018-01-08 21:39:38 +0100

    typo

--- isbg/imaputils.py
+++ isbg/imaputils.py
@@ -21,7 +21,7 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
-"""Imap mofule for isbg."""
+"""Imap module for isbg."""
 
 import email      # To eassily encapsulated emails messages
 import imaplib
