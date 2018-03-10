08387565b653b2677260e9785221249fdd84008a
Carles Muñoz Gorriz <carlesmu@internautas.org>
2018-03-10 16:09:29 +0100

    Enable run tests with using 'python setup.py test'

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         ]
     },
     install_requires=['docopt', 'chardet'],
+    setup_requires=['pytest_runner'],
     tests_require=['pytest', 'mock', 'pytest-cov'],
     url='https://github.com/isbg/isbg',
     classifiers=[
