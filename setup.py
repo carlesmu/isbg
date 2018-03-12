ed725062f601cfbfc842ee7771dd4c179fb23cf9
Carles Muñoz Gorriz <carlesmu@internautas.org>
2018-03-12 17:18:08 +0100

    Create documentation and manpage at build time

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Installation and build information for isbg."""
 
 import ast
 import os
 import re
+import subprocess
 import sys
 
+from distutils.command.build import build as _build
 from setuptools import setup
+
+
+class Build(_build):
+    """Customize setuptools build command."""
+
+    def run(self):
+        """Create the docs (html and manpage)."""
+        ret = subprocess.call(['make', 'docs'])
+        if ret != 0:
+            sys.stderr.write(u"Error in call to «make doc»")
+            sys.exit(ret)
+        _build.run(self)
+
 
 # We get the isbg README.rst as package long description:
 with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'rb') as f:
@@ -40,6 +55,9 @@ setup(
             'isbg = isbg.__main__:main',
             'sa_unwrap = isbg.sa_unwrap:run',
         ]
+    },
+    cmdclass={
+        'build': Build,
     },
     install_requires=['docopt'],
     extras_require={
